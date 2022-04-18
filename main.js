be39ce06a3d9c43a0051dfc4cf8adbbc88f8388d
Mwai <machariamwai@gmail.com>
2022-04-18 21:52:21 +0300

    Pass all tests

--- main.js
+++ main.js
@@ -3,8 +3,49 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.querySelector('#modal')
+modal.setAttribute('Class','hidden')
+
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
+function switchHearts(heart){
+  if(heart.className === "like-glyph"){
+    heart.innerHTML = FULL_HEART
+    heart.className = "like-glyph activated-heart"
+  } else if (heart.className === 'like-glyph activated-heart'){
+    heart.innerHTML = EMPTY_HEART
+    heart.className = 'like-glyph'
+  }
+}
+
+function visibleModal(){
+  modal.className = "hidden";
+}
 
 
 //------------------------------------------------------------------------------
